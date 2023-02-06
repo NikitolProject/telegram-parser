5871b6497bb8038a475df1157382a8e3e71a467d
Nikita Tolstov <tolnikita556@gmail.com>
2023-02-07 01:46:35 +0300

    small fixes

--- app/tasks.py
+++ app/tasks.py
@@ -149,6 +149,7 @@ def get_telegram_channels_by_ids(channel_ids: List[int]) -> List[int]:
 
 @database_sync_to_async
 def get_telegram_users_by_ids(user_ids: List[int]) -> List[int]:
+    print(user_ids)
     return [obj.user_id for obj in TelegramUser.objects.in_bulk(user_ids).values()]
 
 
