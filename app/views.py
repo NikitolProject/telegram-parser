b0f08c060abdf2d9633243ef0844fa34587835b5
Nikita Tolstov <tolnikita556@gmail.com>
2023-02-07 15:42:03 +0300

    small fixes

--- app/views.py
+++ app/views.py
@@ -87,6 +87,7 @@ def run_async_parsing_loop(loop, request, form):
 
 
 def run_async_mailing_loop(loop, request, form, file):
+    print(request.POST['_selected_action'])
     asyncio.set_event_loop(loop)
     loop.run_until_complete(
         start_mailing(
